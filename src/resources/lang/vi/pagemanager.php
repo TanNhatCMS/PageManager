1efd8877aacf5be91365ac9beb8c98260c700afc
TanNhatCMS <mrkiet.edu@gmail.com>
2023-01-31 13:12:17 +0700

    thêm tiếng việt

---
+++ src/resources/lang/vi/pagemanager.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'change_template_confirmation' => 'Bạn có chắc chắn muốn thay đổi mẫu trang không? Bạn sẽ mất mọi sửa đổi chưa được lưu cho trang này.',
+    'content' => 'Nội dung',
+    'content_placeholder' => 'Nội dung của bạn ở đây',
+    'meta_description' => 'Meta mô tả',
+    'meta_keywords' => 'Meta từ khóa',
+    'meta_title' => 'Meta tiêu đề',
+    'metas' => 'Metas',
+    'name' => 'Tên',
+    'open' => 'Mở',
+    'page' => 'page',
+    'page_name' => 'Tên trang (chỉ quản trị viên nhìn thấy)',
+    'page_slug' => 'Đường dẫn tĩnh trang(URL)',
+    'page_slug_hint' => 'Sẽ được tạo tự động từ tiêu đề của bạn, nếu để trống.',
+    'page_title' => 'Tiêu đề trang',
+    'pages' => 'pages',
+    'slug' => 'Đường dẫn tĩnh',
+    'template' => 'Mẫu Template',
+    'template_not_found' => 'Không thể tìm thấy mẫu. Nó có thể đã bị xóa kể từ khi trang này được tạo. Để tiếp tục, vui lòng yêu cầu webmin hoặc nhóm phát triển của bạn khắc phục sự cố này.',
+];
